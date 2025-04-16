4ea383ba91d90683899d5aef758f4956c5e91a53
LucasNarZ <LucasNarloch123@gmail.com>
2025-04-16 11:52:36 -0300

    declaring stacks

--- main.c
+++ main.c
@@ -2,6 +2,9 @@
 #include "./lexer/lexer.h"
 #include "./parser/parser.h"
 
+Stack stack = {0};
+ScopeStack scopesStack;
+
 
 int main(){
     FILE *file_ptr = fopen("input.txt", "r");
@@ -11,6 +14,8 @@ int main(){
         getTokens(&head, &input);
     }
     Node *root = createNode("ROOT", "ROOT");
+    scopesStack.scope = malloc(MAX_SIZE_SCOPE * sizeof(int));;
+    scopesStack.scope[0] = 0;
     parseBlock(root, &head);
     printTreePretty(root);
     return 0;

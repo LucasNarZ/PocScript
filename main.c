e441d05083e990e7d8d126de6ebdd60c7b2b2e0c
LucasNarZ <LucasNarloch123@gmail.com>
2025-04-18 10:43:38 -0300

    .ps to read file

--- main.c
+++ main.c
@@ -7,12 +7,13 @@ ScopeStack scopesStack;
 
 
 int main(){
-    FILE *file_ptr = fopen("input.txt", "r");
+    FILE *file_ptr = fopen("input.ps", "r");
     char file[100];
     while(fgets(file, 100, file_ptr)){
         char *input = file;
         getTokens(&head, &input);
     }
+    print(head);
     Node *root = createNode("ROOT", "ROOT");
     scopesStack.scope = malloc(MAX_SIZE_SCOPE * sizeof(int));;
     scopesStack.scope[0] = 0;
